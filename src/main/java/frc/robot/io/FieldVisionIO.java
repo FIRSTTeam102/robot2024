359ca5a0dd677b2f535f18f928cf18e1119dc00f
Brayden N <gamerinfusion@gmail.com>
2024-01-22 19:08:29 -0500

    "finished" vision.io

--- src/main/java/frc/robot/io/FieldVisionIO.java
+++ src/main/java/frc/robot/io/FieldVisionIO.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.io;
 
 import edu.wpi.first.networktables.NetworkTable;
@@ -11,92 +7,84 @@ import edu.wpi.first.wpilibj.DriverStation;
 
 import org.littletonrobotics.junction.AutoLog;
 
-/** Add your docs here. */
 public class FieldVisionIO {
 	@AutoLog
 	public static class FieldVisonIOInputs {
-		// basic limelight configuration settings
-		public long fieldVisionPipeline = 0;
-		public boolean fieldVisionTarget = false;
-		public long fieldVisionTargetAprilTag = 0;
+		public int pipeline = 0;
+		public boolean hasTarget = false;
+		public int targetAprilTag = 0;
 
-		// basic limelight target info
-		public double fieldVisionCrosshairToTargetErrorX_rad = 0.0;
-		public double fieldVisionCrosshairToTargetErrorY_rad = 0.0;
-		public double fieldVisionTargetArea = 0.0;
+		public double crosshairToTargetErrorX_rad = 0.0;
+		public double crosshairToTargetErrorY_rad = 0.0;
+		public double targetArea = 0.0;
 
-		// Field orientations
-		// "Where am I in relation to my target apriltag"
-		public double fieldVisionBotpose_TargetspaceTranslationX_m = 0.0;
-		public double fieldVisionBotpose_TargetspaceTranslationY_m = 0.0;
-		public double fieldVisionBotpose_TargetspaceTranslationZ_m = 0.0;
-		public double fieldVisionBotpose_TargetspaceRotationX_rad = 0.0;
-		public double fieldVisionBotpose_TargetspaceRotationY_rad = 0.0;
-		public double fieldVisionBotpose_TargetspaceRotationZ_rad = 0.0;
-		// "Where am I on the field"
-		public double fieldVisionBotpose_FieldspaceTranslationX_m = 0.0;
-		public double fieldVisionBotpose_FieldspaceTranslationY_m = 0.0;
-		public double fieldVisionBotpose_FieldspaceTranslationZ_m = 0.0;
-		public double fieldVisionBotpose_FieldspaceRotationX_rad = 0.0;
-		public double fieldVisionBotpose_FieldspaceRotationY_rad = 0.0;
-		public double fieldVisionBotpose_FieldspaceRotationZ_rad = 0.0;
+		public double targetspaceTranslationX_m = 0.0;
+		public double targetspaceTranslationY_m = 0.0;
+		public double targetspaceTranslationZ_m = 0.0;
+		public double targetspaceRotationX_rad = 0.0;
+		public double targetspaceRotationY_rad = 0.0;
+		public double targetspaceRotationZ_rad = 0.0;
 
-		public double fieldVisionBotpose_Latency_s = 0.0; // Used in the estimation of our pose whilst considering lag
+		public double fieldspaceTranslationX_m = 0.0;
+		public double fieldspaceTranslationY_m = 0.0;
+		public double fieldspaceTranslationZ_m = 0.0;
+		public double fieldspaceRotationX_rad = 0.0;
+		public double fieldspaceRotationY_rad = 0.0;
+		public double fieldspaceRotationZ_rad = 0.0;
+
+		public double fieldspaceTotalLatency_s = 0.0;
 	}
 
-	// methods to get information from LL
-	private double tl = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tl").getDouble(0);
-	private double cl = NetworkTableInstance.getDefault().getTable("limelight").getEntry("cl").getDouble(0);
+	private NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight-fv");
 
-	// Basic targeting info
-	private NetworkTable tableFieldVision = NetworkTableInstance.getDefault().getTable("limelight-fv");
+	private NetworkTableEntry pipelineEntry = table.getEntry("pipeline");
+	private NetworkTableEntry tvEntry = table.getEntry("tv");
+	private NetworkTableEntry tidEntry = table.getEntry("tid");
 
-	private NetworkTableEntry pipelineEntryFieldVision = tableFieldVision.getEntry("pipeline");
-	private NetworkTableEntry tvEntryFieldVision = tableFieldVision.getEntry("tv");
-	private NetworkTableEntry tidEntryFieldVision = tableFieldVision.getEntry("tid");
+	private NetworkTableEntry txEntry = table.getEntry("tx");
+	private NetworkTableEntry tyEntry = table.getEntry("ty");
+	private NetworkTableEntry taEntry = table.getEntry("ta");
 
-	private NetworkTableEntry txEntryFieldVision = tableFieldVision.getEntry("tx");
-	private NetworkTableEntry tyEntryFieldVision = tableFieldVision.getEntry("ty");
-	private NetworkTableEntry taEntryFieldVision = tableFieldVision.getEntry("ta");
+	private NetworkTableEntry clEntry = table.getEntry("cl");
+	private NetworkTableEntry tlEntry = table.getEntry("tl");
 
-	// 3D data
-	private NetworkTableEntry botpose_targetspaceEntryFieldVision = tableFieldVision.getEntry("botpose_targetspace");
-	private double[] botpose_targetspaceCacheFieldVision = new double[6]; // The array that will hold all the position
+	private NetworkTableEntry targetspaceEntry = table.getEntry("targetspace");
+	private double[] targetspaceCache = new double[6]; // array that will hold all the positions
 
-	private NetworkTableEntry botpose_wpiblueEntryFieldVision = tableFieldVision.getEntry("botpose_wpiblue");
-	private double[] botpose_wpiblueCacheFieldVision = new double[7];
+	private NetworkTableEntry botpose_wpiblueEntry = table.getEntry("botpose_wpiblue");
+	private double[] botpose_wpiblueCache = new double[7];
 
 	public void updateInputs(FieldVisonIOInputs inputs) {
-		inputs.fieldVisionPipeline = pipelineEntryFieldVision.getNumber(inputs.fieldVisionPipeline).intValue();
-		inputs.fieldVisionTarget = tvEntryFieldVision.getDouble(0) == 1;
-		inputs.fieldVisionTargetAprilTag = tidEntryFieldVision.getNumber(inputs.fieldVisionTargetAprilTag).intValue();
-		inputs.fieldVisionCrosshairToTargetErrorX_rad = Math
-			.toRadians(txEntryFieldVision.getDouble(inputs.fieldVisionCrosshairToTargetErrorX_rad));
-		inputs.fieldVisionCrosshairToTargetErrorY_rad = Math
-			.toRadians(tyEntryFieldVision.getDouble(inputs.fieldVisionCrosshairToTargetErrorX_rad));
-		inputs.fieldVisionTargetArea = taEntryFieldVision.getDouble(inputs.fieldVisionTargetArea);
+		inputs.pipeline = pipelineEntry.getNumber(inputs.pipeline).intValue();
+		inputs.hasTarget = tvEntry.getDouble(0) == 1;
+		inputs.targetAprilTag = tidEntry.getNumber(inputs.targetAprilTag).intValue();
+		inputs.crosshairToTargetErrorX_rad = Math.toRadians(txEntry.getDouble(inputs.crosshairToTargetErrorX_rad));
+		inputs.crosshairToTargetErrorY_rad = Math.toRadians(tyEntry.getDouble(inputs.crosshairToTargetErrorX_rad));
+		inputs.targetArea = taEntry.getDouble(inputs.targetArea);
 
-		// Log all input
-		botpose_targetspaceCacheFieldVision = botpose_targetspaceEntryFieldVision
-			.getDoubleArray(botpose_targetspaceCacheFieldVision);
-		if (botpose_targetspaceCacheFieldVision.length > 0) {
-			inputs.fieldVisionBotpose_TargetspaceTranslationX_m = botpose_targetspaceCacheFieldVision[0];
-			inputs.fieldVisionBotpose_TargetspaceTranslationY_m = botpose_targetspaceCacheFieldVision[1];
-			inputs.fieldVisionBotpose_TargetspaceTranslationZ_m = botpose_targetspaceCacheFieldVision[2];
-			inputs.fieldVisionBotpose_TargetspaceRotationX_rad = Math.toRadians(botpose_targetspaceCacheFieldVision[3]);
-			inputs.fieldVisionBotpose_TargetspaceRotationY_rad = Math.toRadians(botpose_targetspaceCacheFieldVision[4]);
-			inputs.fieldVisionBotpose_TargetspaceRotationZ_rad = Math.toRadians(botpose_targetspaceCacheFieldVision[5]);
-			inputs.fieldVisionBotpose_Latency_s = botpose_wpiblueCacheFieldVision[6] / 1000;
+		targetspaceCache = targetspaceEntry.getDoubleArray(targetspaceCache);
+		if (targetspaceCache.length > 0) {
+			inputs.targetspaceTranslationX_m = targetspaceCache[0];
+			inputs.targetspaceTranslationY_m = targetspaceCache[1];
+			inputs.targetspaceTranslationZ_m = targetspaceCache[2];
+			inputs.targetspaceRotationX_rad = Math.toRadians(targetspaceCache[3]);
+			inputs.targetspaceRotationY_rad = Math.toRadians(targetspaceCache[4]);
+			inputs.targetspaceRotationZ_rad = Math.toRadians(targetspaceCache[5]);
 		} else
-			DriverStation.reportWarning("Invalid botpose array from limelight", true);
+			DriverStation.reportWarning("invalid targetspace array from limelight", true);
 
-		botpose_wpiblueCacheFieldVision = botpose_wpiblueEntryFieldVision.getDoubleArray(botpose_wpiblueCacheFieldVision);
-		inputs.fieldVisionBotpose_FieldspaceTranslationX_m = botpose_wpiblueCacheFieldVision[0];
-		inputs.fieldVisionBotpose_FieldspaceTranslationY_m = botpose_wpiblueCacheFieldVision[1];
-		inputs.fieldVisionBotpose_FieldspaceTranslationZ_m = botpose_wpiblueCacheFieldVision[2];
-		inputs.fieldVisionBotpose_FieldspaceRotationX_rad = Math.toRadians(botpose_wpiblueCacheFieldVision[3]);
-		inputs.fieldVisionBotpose_FieldspaceRotationY_rad = Math.toRadians(botpose_wpiblueCacheFieldVision[4]);
-		inputs.fieldVisionBotpose_FieldspaceRotationZ_rad = Math.toRadians(botpose_wpiblueCacheFieldVision[5]);
-		inputs.fieldVisionBotpose_Latency_s = botpose_wpiblueCacheFieldVision[6] / 1000;
+		if (botpose_wpiblueCache.length > 0) {
+			botpose_wpiblueCache = botpose_wpiblueEntry.getDoubleArray(botpose_wpiblueCache);
+			inputs.fieldspaceTranslationX_m = botpose_wpiblueCache[0];
+			inputs.fieldspaceTranslationY_m = botpose_wpiblueCache[1];
+			inputs.fieldspaceTranslationZ_m = botpose_wpiblueCache[2];
+			inputs.fieldspaceRotationX_rad = Math.toRadians(botpose_wpiblueCache[3]);
+			inputs.fieldspaceRotationY_rad = Math.toRadians(botpose_wpiblueCache[4]);
+			inputs.fieldspaceRotationZ_rad = Math.toRadians(botpose_wpiblueCache[5]);
+			inputs.fieldspaceTotalLatency_s = botpose_wpiblueCache[6] / 1000;
+		} else {
+			DriverStation.reportWarning("invalid wpiblue array from limelight", true);
+			inputs.fieldspaceTotalLatency_s = (tlEntry.getDouble(0) - clEntry.getDouble(0)) / 1000;
+		}
 	}
 }
