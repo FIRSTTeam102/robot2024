e2eabd07e2b973d4b0e0e7f7d6afcb03cd257a75
RushaliB <rushalibetala@gmail.com>
2024-01-22 20:06:42 -0500

    added advantage kit inputs

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -8,6 +8,9 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 
+import org.littletonrobotics.junction.AutoLog;
+import org.littletonrobotics.junction.Logger;
+
 public class Intake extends SubsystemBase {
 	private final CANSparkMax motor = new CANSparkMax(motorId, MotorType.kBrushless);
 
@@ -17,16 +20,29 @@ public class Intake extends SubsystemBase {
 
 	@Override
 	public void periodic() {
-	}
-
-	public boolean checkSensor() {
-		return noteSensor.get();
+		updateInputs(inputs);
+		Logger.processInputs(getName(), inputs);
 	}
 
 	public void setMotorSpeed(double speed) {
 		motor.set(speed);
-		return;
 	}
 
-	public void 
+	@AutoLog
+	public static class IntakeIOInputs {
+		public double current_A = 0.0;
+		public double voltage_V = 0.0;
+		public double tempature_C = 0.0;
+		public boolean noteSensor = false;
+	}
+
+	public IntakeIOInputsAutoLogged inputs = new IntakeIOInputsAutoLogged();
+
+	public void updateInputs(IntakeIOInputs inputs) {
+		inputs.current_A = motor.getOutputCurrent();
+		inputs.voltage_V = motor.getBusVoltage();
+		inputs.tempature_C = motor.getMotorTemperature();
+		inputs.noteSensor = noteSensor.get();
+	}
+
 }
