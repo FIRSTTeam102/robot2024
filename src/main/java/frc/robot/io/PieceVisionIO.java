3f592cdb1f60c1cd4a9be17b836b1979c028535a
Brayden N <gamerinfusion@gmail.com>
2024-02-14 19:52:19 -0500

    Made gamepiece part of vision

--- src/main/java/frc/robot/io/PieceVisionIO.java
+++ src/main/java/frc/robot/io/PieceVisionIO.java
@@ -1,3 +1,7 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.io;
 
 import edu.wpi.first.networktables.NetworkTable;
@@ -7,9 +11,10 @@ import edu.wpi.first.wpilibj.DriverStation;
 
 import org.littletonrobotics.junction.AutoLog;
 
-public class FieldVisionIO {
+/** Add your docs here. */
+public class PieceVisionIO {
 	@AutoLog
-	public class FieldVisionIOInputs {
+	public class PieceVisionIOInputs {
 		public int pipeline = 0;
 		public boolean hasTarget = false;
 		public int targetAprilTag = 0;
@@ -35,7 +40,8 @@ public class FieldVisionIO {
 		public double fieldspaceTotalLatency_s = 0.0;
 	}
 
-	private NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight-fv");
+	// Network table declarations
+	private NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight-gp");
 
 	private NetworkTableEntry pipelineEntry = table.getEntry("pipeline");
 	private NetworkTableEntry tvEntry = table.getEntry("tv");
@@ -54,7 +60,7 @@ public class FieldVisionIO {
 	private NetworkTableEntry botpose_wpiblueEntry = table.getEntry("botpose_wpiblue");
 	private double[] botpose_wpiblueCache = new double[7];
 
-	public void updateInputs(FieldVisionIOInputs inputs) {
+	public void updateInputs(PieceVisionIOInputs inputs) {
 		inputs.pipeline = pipelineEntry.getNumber(inputs.pipeline).intValue();
 		inputs.hasTarget = tvEntry.getDouble(0) == 1;
 		inputs.targetAprilTag = tidEntry.getNumber(inputs.targetAprilTag).intValue();
@@ -83,7 +89,7 @@ public class FieldVisionIO {
 			inputs.fieldspaceRotationZ_rad = Math.toRadians(botpose_wpiblueCache[5]);
 			inputs.fieldspaceTotalLatency_s = botpose_wpiblueCache[6] / 1000;
 		} else {
-			DriverStation.reportWarning("(Field Vision) invalid wpiblue array from limelight", true);
+			DriverStation.reportWarning("(Piece Vision)invalid wpiblue array from limelight", true);
 			inputs.fieldspaceTotalLatency_s = (tlEntry.getDouble(0) - clEntry.getDouble(0)) / 1000;
 		}
 	}
