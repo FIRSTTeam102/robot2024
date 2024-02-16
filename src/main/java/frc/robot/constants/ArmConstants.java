ac1abebff25efca70e9be1163f53c16497c92eb7
Sean Maizel <robosean00@gmail.com>
2024-02-16 17:52:27 -0500

    Updated arm to use 2 motors and throughbore absolute encoder

--- src/main/java/frc/robot/constants/ArmConstants.java
+++ src/main/java/frc/robot/constants/ArmConstants.java
@@ -1,15 +1,18 @@
 package frc.robot.constants;
 
 public final class ArmConstants {
-	public static final int motorId = 2; // placeholder
+	public static final int leadMotorId = 29;
+	public static final int followerMotorId = 30;
+
+	public static final double shaftEncoderOffset_deg = 0.0; // needs to be tuned
 
 	// pid
 	public static final double kP = 0;
 	public static final double kI = 0;
 	public static final double kD = 0;
 
-	public static final double minOutput = -1;
-	public static final double maxOutput = 1;
+	public static final double maxOutput = .7;
+	public static final double minOutput = -maxOutput;
 
 	// feedforward
 	public static final double kS = 0;
@@ -20,12 +23,12 @@ public final class ArmConstants {
 	public static final double verticalArmPos_deg = 90;
 
 	// smartmotion (all values are for the motor, not the arm)
-	public static double maxAccel_rpmps = 400;
-	public static double maxVelocity_rpm = 800;
+	public static double maxAccel_rpmps = 100;
+	public static double maxVelocity_rpm = 50;
 
 	// Conversion
 	public static final int gearRatio = 1; // 1 IS PLACEHOLDER
 
 	// closeEnough
-	public static final double closeVar = 0.1;
+	public static final double accuracyTolerance = 0.1;
 }
